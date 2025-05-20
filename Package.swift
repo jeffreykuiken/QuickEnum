7cb25ecb93ecc25d17f71e8d81b5b72e0ec8d098
Jeffrey <5014841+jeffreykuiken@users.noreply.github.com>
2025-05-20 17:15:35 +0200

    Remove QuickEnumClient as product and target

--- Package.swift
+++ Package.swift
@@ -13,10 +13,6 @@ let package = Package(
             name: "QuickEnum",
             targets: ["QuickEnum"]
         ),
-        .executable(
-            name: "QuickEnumClient",
-            targets: ["QuickEnumClient"]
-        ),
     ],
     dependencies: [
         .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest"),
@@ -35,9 +31,6 @@ let package = Package(
 
         // Library that exposes a macro as part of its API, which is used in client programs.
         .target(name: "QuickEnum", dependencies: ["QuickEnumMacros"]),
-
-        // A client of the library, which is able to use the macro in its own code.
-        .executableTarget(name: "QuickEnumClient", dependencies: ["QuickEnum"]),
 
         // A test target used to develop the macro implementation.
         .testTarget(
